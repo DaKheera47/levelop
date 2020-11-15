b04d978a790420b33708b70ed44bc4c0fc60d339
DaKheera47 <shaheergoodboy@gmail.com>
2020-11-15 10:50:12 +0500

    Initial Commit

---
+++ src/components/organisms/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import LogoSvg from "../../atoms/logo";
+
+import "./Nav.sass";
+
+export default function NavBar() {
+    return (
+        <nav className="navbar">
+            <LogoSvg />
+        </nav>
+    );
+}
---
+++ src/components/templates/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NavBar from "../organisms/Nav/Nav";
+
+export default function Home() {
+    return (
+        <div>
+            <NavBar />
+        </div>
+    );
+}
