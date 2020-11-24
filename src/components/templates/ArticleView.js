e4cd03d922e2ea28ccb54497bbca1147bd86c65f
DaKheera47 <shaheergoodboy@gmail.com>
2020-11-24 12:44:39 +0500

    Adding article content

--- src/components/templates/ArticleView.js
+++ src/components/templates/ArticleView.js
@@ -17,6 +17,30 @@ export default function ArticleView() {
                 title="How to learn nodejs and mongodb from scratch!"
                 profileUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png"
             />
+
+            <div className="article-content">
+                <p>
+                    Amet minim mollit non deserunt ullamco est sit aliqua dolor
+                    do amet sint. Velit officia consequat duis enim velit
+                    mollit. Exercitation veniam consequat sunt nostrud amet.
+                </p>
+            </div>
+
+            <div className="article-content">
+                <p>
+                    Amet minim mollit non deserunt ullamco est sit aliqua dolor
+                    do amet sint. Velit officia consequat duis enim velit
+                    mollit. Exercitation veniam consequat sunt nostrud amet.
+                </p>
+            </div>
+
+            <div className="article-content">
+                <p>
+                    Amet minim mollit non deserunt ullamco est sit aliqua dolor
+                    do amet sint. Velit officia consequat duis enim velit
+                    mollit. Exercitation veniam consequat sunt nostrud amet.
+                </p>
+            </div>
         </motion.div>
     );
 }
