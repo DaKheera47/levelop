c0c3164936e1bf4e3b9f66710d70c131c547eed9
DaKheera47 <shaheergoodboy@gmail.com>
2020-11-24 12:39:36 +0500

    Redirect to home page by clicking nav-logo

--- src/components/organisms/Nav/Nav.js
+++ src/components/organisms/Nav/Nav.js
@@ -1,17 +1,26 @@
 import { BiSearchAlt2 } from "react-icons/bi";
 
+import { useHistory } from "react-router-dom";
 import LogoSvg from "../../atoms/logo";
 import SideBar from "../SideBar/SideBar";
 
 import "./navStyle.sass";
 
 export default function NavBar() {
+    const history = useHistory();
+
+    const handleLogoClick = () => {
+        history.push("/");
+    };
+
     return (
         <nav className="navbar">
             <div className="navbar-hamburger">
                 <SideBar />
             </div>
-            <LogoSvg />
+            <div className="navbar-logo" onClick={handleLogoClick}>
+                <LogoSvg />
+            </div>
             <BiSearchAlt2 size={23} className="navbar-search" color="white" />
         </nav>
     );
