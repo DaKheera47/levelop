fc5dc913c257bd3373adf2cac50594d49b887c9c
DaKheera47 <shaheergoodboy@gmail.com>
2020-11-25 08:51:04 +0500

    Adding date in the same line as comment author

--- src/components/molecules/Comment/Comment.js
+++ src/components/molecules/Comment/Comment.js
@@ -7,17 +7,19 @@ export default function Comment({ author, content }) {
         <div>
             <hr color="#fa6400" size="1" />
             <div className="comment-body">
-                <div>
+                <div className="comment-profileUrl-container">
                     <img
                         className="comment-profileUrl"
                         src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png"
                         alt=""
                     />
                 </div>
-                <div>
+
+                <div className="comment-header">
                     <Title className="comment-author" text={author} />
-                    <p className="comment-content">{content}</p>
+                    <span className="comment-date">24/11/20</span>
                 </div>
+                <p className="comment-content">{content}</p>
             </div>
         </div>
     );
