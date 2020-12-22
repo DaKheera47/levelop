fa7942f04f383a5593936114063072b141d36519
DaKheera47 <shaheergoodboy@gmail.com>
2020-12-22 14:19:29 +0500

    Working towards left panel

---
+++ src/components/molecules/HomeProfile/HomeProfile.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Nibble from "../../atoms/Nibble";
+
+import "./HomeProfile.sass";
+
+export default function SideBarProfile({
+    imageSrc,
+    profileName,
+    profileHandle,
+    profileNibbles,
+}) {
+    return (
+        <div className="home-profile-profile-container">
+            <img
+                src={imageSrc}
+                alt="Profile"
+                className="home-profile-profileUrl"
+            />
+            <div className="home-profile-profile-info-container">
+                <p className="home-profile-profile-name">{profileName}</p>
+                <p className="home-profile-profile-handle">
+                    {profileHandle}
+                    <span className="home-profile-profile-nibbles">
+                        <Nibble className="home-profile-profile-nibble-logo" />
+                        {profileNibbles}
+                    </span>
+                </p>
+            </div>
+        </div>
+    );
+}
--- src/components/templates/Home.js
+++ src/components/templates/Home.js
@@ -1,11 +1,53 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import Post from "../molecules/Post/Post";
+import HomeProfile from "../molecules/HomeProfile/HomeProfile";
+import { useMediaQuery } from "react-responsive";
 
 import "./homeStyle.sass";
-
 export default function Home() {
+    const isDesktopOrLaptop = useMediaQuery({
+        query: "(min-device-width: 1224px)",
+    });
     return (
         <div className="home-page">
+            {isDesktopOrLaptop && (
+                <div className="home-left-panel">
+                    <div className="profile">
+                        <HomeProfile
+                            imageSrc="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
+                            profileHandle="@VIBEGANG"
+                            profileName="Horse Ammar"
+                            profileNibbles={50}
+                        />
+                    </div>
+
+                    <div className="home-left-panel-links-container">
+                        <NavLink
+                            to="/"
+                            className="home-panel-link"
+                            activeClassName="home-panel-link-active"
+                        >
+                            Feed
+                        </NavLink>
+                        <NavLink
+                            to="/saved"
+                            className="home-panel-link"
+                            activeClassName="home-panel-link-active"
+                        >
+                            Saved
+                        </NavLink>
+                        <NavLink
+                            to="/settings"
+                            className="home-panel-link"
+                            activeClassName="home-panel-link-active"
+                        >
+                            Settings
+                        </NavLink>
+                    </div>
+                </div>
+            )}
+
             <div className="posts">
                 <Post
                     author="Zain Abbas"
