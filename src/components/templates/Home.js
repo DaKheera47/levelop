68eddbd1c22d1e82230cf79fdd64b46d9228396d
DaKheera47 <shaheergoodboy@gmail.com>
2020-12-28 08:02:25 +0500

    Solving some eslint errors

--- src/components/templates/Home.js
+++ src/components/templates/Home.js
@@ -15,63 +15,64 @@ export default function Home() {
 
     useEffect(() => {
         getAllPosts();
+        // removing eslint error as we dont need to constantly get new posts
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
-        <>
-            <div className="home-page">
-                {isDesktopOrLaptop && (
-                    <div className="home-left-panel">
-                        <div className="profile">
-                            <HomeProfile
-                                imageSrc="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
-                                profileHandle="@VIBEGANG"
-                                profileName="Horse Ammar"
-                                profileNibbles={50}
-                            />
-                        </div>
+        <div className="home-page">
+            {isDesktopOrLaptop && (
+                <div className="home-left-panel">
+                    <div className="profile">
+                        <HomeProfile
+                            imageSrc="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
+                            profileHandle="@VIBEGANG"
+                            profileName="Horse Ammar"
+                            profileNibbles={50}
+                        />
+                    </div>
 
-                        <div className="home-left-panel-links-container">
-                            <NavLink
-                                to="/"
-                                className="home-panel-link"
-                                activeClassName="home-panel-link-active"
-                            >
-                                Feed
-                            </NavLink>
-                            <NavLink
-                                to="/saved"
-                                className="home-panel-link"
-                                activeClassName="home-panel-link-active"
-                            >
-                                Saved
-                            </NavLink>
-                            <NavLink
-                                to="/settings"
-                                className="home-panel-link"
-                                activeClassName="home-panel-link-active"
-                            >
-                                Settings
-                            </NavLink>
-                        </div>
+                    <div className="home-left-panel-links-container">
+                        <NavLink
+                            to="/"
+                            className="home-panel-link"
+                            activeClassName="home-panel-link-active"
+                        >
+                            Feed
+                        </NavLink>
+                        <NavLink
+                            to="/saved"
+                            className="home-panel-link"
+                            activeClassName="home-panel-link-active"
+                        >
+                            Saved
+                        </NavLink>
+                        <NavLink
+                            to="/settings"
+                            className="home-panel-link"
+                            activeClassName="home-panel-link-active"
+                        >
+                            Settings
+                        </NavLink>
                     </div>
-                )}
-                {isLoading ? (
-                    <p>Loading</p>
-                ) : (
-                    <div className="posts">
-                        {allPosts["data"]?.map((e) => (
-                            <Post
-                                author={e?.author?.username}
-                                title={e.title}
-                                imgSrc="https://www.online-tech-tips.com/wp-content/uploads/2018/10/touch-typing-tips.jpg"
-                                imgAlt="image"
-                                postUrl={`posts/${e._id}`}
-                            />
-                        ))}
-                    </div>
-                )}
-            </div>
-        </>
+                </div>
+            )}
+            {isLoading ? (
+                <p>Loading</p>
+            ) : (
+                <div className="posts">
+                    {allPosts["data"]?.map((e) => (
+                        <Post
+                            key={e?._id}
+                            author={e?.author?.username}
+                            title={e?.title}
+                            imgSrc="https://www.online-tech-tips.com/wp-content/uploads/2018/10/touch-typing-tips.jpg"
+                            imgAlt="image"
+                            postUrl={`posts/${e?._id}`}
+                        />
+                    ))}
+                </div>
+            )}
+        </div>
     );
 }
