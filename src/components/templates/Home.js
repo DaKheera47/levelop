896cba718318be6d753bb9029c4ae9e87cdcd683
DaKheera47 <shaheergoodboy@gmail.com>
2020-12-28 18:10:32 +0500

    Improving responsive design for homepage

--- src/components/templates/Home.js
+++ src/components/templates/Home.js
@@ -4,8 +4,8 @@ import Post from "../molecules/Post/Post";
 import HomeProfile from "../molecules/HomeProfile/HomeProfile";
 import { useMediaQuery } from "react-responsive";
 import { PostsContext } from "../contexts/AllPostsContext";
-
 import "./homeStyle.sass";
+
 export default function Home() {
     const isDesktopOrLaptop = useMediaQuery({
         query: "(min-device-width: 1224px)",
@@ -19,60 +19,62 @@ export default function Home() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    return (
-        <div className="home-page">
-            {isDesktopOrLaptop && (
-                <div className="home-left-panel">
-                    <div className="profile">
-                        <HomeProfile
-                            imageSrc="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
-                            profileHandle="@VIBEGANG"
-                            profileName="Horse Ammar"
-                            profileNibbles={50}
-                        />
-                    </div>
+    const posts = allPosts["data"]?.map((e) => (
+        <Post
+            key={e?._id}
+            author={e?.author?.username}
+            title={e?.title}
+            imgSrc="https://www.online-tech-tips.com/wp-content/uploads/2018/10/touch-typing-tips.jpg"
+            imgAlt="image"
+            postUrl={`posts/${e?._id}`}
+        />
+    ));
 
-                    <div className="home-left-panel-links-container">
-                        <NavLink
-                            to="/"
-                            className="home-panel-link"
-                            activeClassName="home-panel-link-active"
-                        >
-                            Feed
-                        </NavLink>
-                        <NavLink
-                            to="/saved"
-                            className="home-panel-link"
-                            activeClassName="home-panel-link-active"
-                        >
-                            Saved
-                        </NavLink>
-                        <NavLink
-                            to="/settings"
-                            className="home-panel-link"
-                            activeClassName="home-panel-link-active"
-                        >
-                            Settings
-                        </NavLink>
+    return (
+        <>
+            <div className="home-page">
+                {isDesktopOrLaptop && (
+                    <div className="home-left-panel">
+                        <div className="profile">
+                            <HomeProfile
+                                imageSrc="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
+                                profileHandle="@VIBEGANG"
+                                profileName="Horse Ammar"
+                                profileNibbles={50}
+                            />
+                        </div>
+
+                        <div className="home-left-panel-links-container">
+                            <NavLink
+                                to="/"
+                                className="home-panel-link"
+                                activeClassName="home-panel-link-active"
+                            >
+                                Feed
+                            </NavLink>
+                            <NavLink
+                                to="/saved"
+                                className="home-panel-link"
+                                activeClassName="home-panel-link-active"
+                            >
+                                Saved
+                            </NavLink>
+                            <NavLink
+                                to="/settings"
+                                className="home-panel-link"
+                                activeClassName="home-panel-link-active"
+                            >
+                                Settings
+                            </NavLink>
+                        </div>
                     </div>
-                </div>
-            )}
-            {isLoading ? (
-                <p>Loading</p>
-            ) : (
-                <div className="posts">
-                    {allPosts["data"]?.map((e) => (
-                        <Post
-                            key={e?._id}
-                            author={e?.author?.username}
-                            title={e?.title}
-                            imgSrc="https://www.online-tech-tips.com/wp-content/uploads/2018/10/touch-typing-tips.jpg"
-                            imgAlt="image"
-                            postUrl={`posts/${e?._id}`}
-                        />
-                    ))}
-                </div>
-            )}
-        </div>
+                )}
+                {isLoading ? (
+                    <p>Loading</p>
+                ) : (
+                    <div className="posts">{posts}</div>
+                )}
+            </div>
+        </>
     );
 }
