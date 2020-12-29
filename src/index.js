2db3ecc2311fb0015ec8cf358b2c8eb3e9d46a07
DaKheera47 <shaheergoodboy@gmail.com>
2020-12-29 10:40:44 +0500

    Reverting back to using BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.sass";
 
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
     <Router>
