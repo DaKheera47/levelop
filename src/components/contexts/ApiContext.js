6b2a8d5818214b91415705b8b5d3944cacd7f82b
DaKheera47 <shaheergoodboy@gmail.com>
2020-12-31 08:49:16 +0500

    Using curr user as state

--- src/components/contexts/ApiContext.js
+++ src/components/contexts/ApiContext.js
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import axios from "axios";
 import Cookies from "universal-cookie";
 import { useHistory } from "react-router-dom";
@@ -7,10 +7,33 @@ export const ApiContext = createContext();
 
 const ApiContextProvider = (props) => {
     const cookies = new Cookies();
-    const preUrl = "http://ammar-62c6689f.localhost.run";
+    const preUrl = "http://ammar-c55ab810.localhost.run";
     const history = useHistory();
 
+    const [currUser, setCurrUser] = useState({});
+
     axios.defaults.withCredentials = true;
+
+    const SignUp = (email, password, username, fullName) => {
+        axios
+            .post(`${preUrl}/register`, {
+                email: email,
+                password: password,
+                username: username,
+                fullname: fullName,
+            })
+            .then(function (res) {
+                //handle success
+                cookies.set("jwt", res?.data?.token);
+                setCurrUser(res?.data?.newUser);
+                console.log(currUser);
+                history.push("/");
+            })
+            .catch(function (res) {
+                //handle error
+                console.log(res);
+            });
+    };
 
     const Login = (email, password) => {
         console.log(email, password);
@@ -30,27 +53,13 @@ const ApiContextProvider = (props) => {
             .then(function (res) {
                 //handle success
                 cookies.set("jwt", res?.data?.token);
+                setCurrUser({
+                    email: res?.data?.email,
+                    fullName: res?.data?.fullname,
+                    username: res?.data?.username,
+                });
                 console.log(res);
-                history.push("/");
-            })
-            .catch(function (res) {
-                //handle error
-                console.log(res);
-            });
-    };
-
-    const SignUp = (email, password, username, fullName) => {
-        axios
-            .post(`${preUrl}/register`, {
-                email: email,
-                password: password,
-                username: username,
-                fullname: fullName,
-            })
-            .then(function (res) {
-                //handle success
-                cookies.set("jwt", res?.data?.token);
-                console.log(res);
+                console.log(currUser);
                 history.push("/");
             })
             .catch(function (res) {
@@ -60,11 +69,13 @@ const ApiContextProvider = (props) => {
     };
 
     const Logout = () => {
-        cookies.remove("jwt");
+        cookies.set("jwt", "");
     };
 
     return (
-        <ApiContext.Provider value={{ preUrl, Login, Logout, cookies, SignUp }}>
+        <ApiContext.Provider
+            value={{ preUrl, Login, Logout, cookies, SignUp, currUser }}
+        >
             {props.children}
         </ApiContext.Provider>
     );
