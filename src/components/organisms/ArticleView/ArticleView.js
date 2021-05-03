980e982f3415756c05c3b83e5ea5c8e8800febba
Shaheer Sarfaraz <shaheergoodboy@gmail.com>
2021-05-03 11:59:05 +0500

    Removing excess comments

--- src/components/organisms/ArticleView/ArticleView.js
+++ src/components/organisms/ArticleView/ArticleView.js
@@ -1,47 +1,33 @@
 import React from "react";
-import { AiOutlineLike, AiOutlineShareAlt } from "react-icons/ai";
-import { VscBookmark } from "react-icons/vsc";
 import "./ArticleView.sass";
 import ArticleContent from "../../molecules/ArticleContent/ArticleContent";
+// import { AiOutlineLike, AiOutlineShareAlt } from "react-icons/ai";
+// import { VscBookmark } from "react-icons/vsc";
 
 export default function ArticleView() {
-    const socialBtns = (
-        <div className="article-view-social-btns">
-            <AiOutlineLike
-                style={{ margin: "0 5px" }}
-                color="#fa6400"
-                size={30}
-            />
-            <AiOutlineShareAlt
-                style={{ margin: "0 5px" }}
-                color="#fa6400"
-                size={30}
-            />
-            <VscBookmark
-                style={{ margin: "0 5px" }}
-                color="#fa6400"
-                size={30}
-            />
-        </div>
-    );
+    // const socialBtns = (
+    //     <div className="article-view-social-btns">
+    //         <AiOutlineLike
+    //             style={{ margin: "0 5px" }}
+    //             color="#fa6400"
+    //             size={30}
+    //         />
+    //         <AiOutlineShareAlt
+    //             style={{ margin: "0 5px" }}
+    //             color="#fa6400"
+    //             size={30}
+    //         />
+    //         <VscBookmark
+    //             style={{ margin: "0 5px" }}
+    //             color="#fa6400"
+    //             size={30}
+    //         />
+    //     </div>
+    // );
 
     return (
         <div className="article-page">
             <ArticleContent />
-
-            {/* {socialBtns} */}
-
-            {/* {article?.comments && !isCommentsLoading(
-                <>
-                    <ArticleComments
-                        comments={article?.comments}
-                        postId={postId}
-                        newCommentContent={newCommentContent}
-                        setNewCommentContent={setNewCommentContent}
-                        handleNewComment={handleNewComment}
-                    />
-                </>
-            )} */}
         </div>
     );
 }
